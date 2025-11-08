d85aa6de8a92bcba8df2d896e8b0e33effc1cd55
Arun Lanka <arunlanka2001@gmail.com>
2025-11-08 15:08:14 +0530

    Update crude_dashboard.py

--- crude_dashboard.py
+++ crude_dashboard.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/196-S9Z8lHkGPdjtnPf_ZVX7D-SebZmWk
 """
 
-!pip install dash dash-bootstrap-components plotly gunicorn
 
 # Paste this entire block into a Colab cell or save as app.py
 import pandas as pd
